8a6d72d5c20e00e8c14b57393e836b171e01748f
Timothy Marsh <Timothy.D.C.Marsh@gmail.com>
2025-12-05 16:21:32 -0500

    Updated tests

--- tests/testthat/test-fit-basicModels.R
+++ tests/testthat/test-fit-basicModels.R
@@ -2,33 +2,33 @@ test_that("linear models work", {
   x <- c(1:100)
   y <- EuStockMarkets[,2][1:100]
   df <- data.frame(x,y)
-  expect_equal(fit_basicModels(y ~ x, model = "linear", df)$coef[[1]], 1712.976, tolerance = 0.001)
+  expect_equal(fit_simple_model(y ~ x, model = "lm", df)$coef[[1]], 1712.976, tolerance = 0.001)
 })
 
 test_that("linear models work 2", {
   x <- c(1:100)
   y <- EuStockMarkets[,2][1:100]
   df <- data.frame(x,y)
-  expect_equal(fit_basicModels(y ~ x, model = "linear", df)$coef[[2]], -0.1818, tolerance = 0.001)
+  expect_equal(fit_simple_model(y ~ x, model = "lm", df)$coef[[2]], -0.1818, tolerance = 0.001)
 })
 
 test_that("AR models work", {
   x <- c(1:100)
   y <- EuStockMarkets[,2][1:100]
   df <- data.frame(x,y)
-  expect_equal(fit_basicModels(y, model = "AR", df)$coef[[1]], 0.762926, tolerance = 0.001)
+  expect_equal(fit_simple_model(y~x, model = "ar", df, order = 1)$coef[[1]], 0.7590775, tolerance = 0.001)
 })
 
 test_that("AR models work 2", {
   x <- c(1:100)
   y <- EuStockMarkets[,2][1:100]
   df <- data.frame(x,y)
-  expect_equal(fit_basicModels(y, model = "AR", df)$coef[[2]], 1703.930612, tolerance = 0.001)
+  expect_equal(fit_simple_model(y~x, model = "ar", df, order = 1)$coef[[2]], 1706.807, tolerance = 0.001)
 })
 
 test_that("Incorrect model specification works", {
   x <- c(1:100)
   y <- EuStockMarkets[,2][1:100]
   df <- data.frame(x,y)
-  expect_equal(fit_basicModels(y~x, model = "None", df), "Model not recognized")
+  expect_equal(fit_simple_model(y~x, model = "None", df), "Model not recognized")
 })
--- tests/testthat/test-now_simple_models.R
+++ tests/testthat/test-now_simple_models.R
@@ -2,7 +2,8 @@ test_that("linear models work1", {
   x <- c(1:105)
   y <- append(EuStockMarkets[,2][1:100], rep(NA,5))
   df <- data.frame(x,y)
-  expect_equal(now_simple_models(y ~ x, model = "lm", df), 
-               c(1694.610, 1694.428, 1694.246, 1694.064, 1693.883 ), 
-               tolerance = 0.001)
+  pred <- now_simple_models(y ~ x, model = "lm", df)
+  expect_equal(pred,c(1694.610, 1694.428, 1694.246, 1694.064, 1693.883),
+               tolerance = 0.01)
 })
+#c(1694.610, 1694.428, 1694.246, 1694.064, 1693.883 )
