87ea49dff6a912d34ec9af9dfdcbfa8d03d49c95
Timothy Marsh <Timothy.D.C.Marsh@gmail.com>
2025-12-09 15:33:44 -0500

    Updated tests

--- tests/testthat/test-now_simple_models.R
+++ tests/testthat/test-now_simple_models.R
@@ -1,19 +1,32 @@
 test_that("linear models work1", {
   x <- c(1:105)
-  y <- append(EuStockMarkets[,2][1:100], rep(NA,5))
-  df <- data.frame(x,y)
+  y <- append(EuStockMarkets[, 2][1:100], rep(NA, 5))
+  df <- data.frame(x, y)
   pred <- now_simple_models(y ~ x, model = "lm", df)
   preds <- c(pred[[1]], pred[[2]], pred[[3]], pred[[4]], pred[[5]])
-  expect_equal(preds,c(1694.610, 1694.428, 1694.246, 1694.064, 1693.883),
-               tolerance = 0.01)
+  expect_equal(preds, c(1694.610, 1694.428, 1694.246, 1694.064, 1693.883),
+    tolerance = 0.01
+  )
 })
 
 test_that("ar models work1", {
   x <- c(1:105)
-  y <- append(EuStockMarkets[,2][1:100], rep(NA,5))
-  df <- data.frame(x,y)
+  y <- append(EuStockMarkets[, 2][1:100], rep(NA, 5))
+  df <- data.frame(x, y)
   pred <- now_simple_models(y ~ x, model = "ar", df, order = 1)
   preds <- c(pred$pred[1], pred$pred[2], pred$pred[3], pred$pred[4], pred$pred[5])
-  expect_equal(preds,c(1726.098, 1720.011, 1715.376, 1711.845, 1709.150),
-               tolerance = 0.01)
+  expect_equal(preds, c(1726.098, 1720.011, 1715.376, 1711.845, 1709.150),
+    tolerance = 0.01
+  )
+})
+
+test_that("NA's handled correctly", {
+  x <- c(1, 1:105)
+  y <- append(NA, append(EuStockMarkets[, 2][1:100], rep(NA, 5)))
+  df <- data.frame(x, y)
+  pred <- now_simple_models(y ~ x, model = "ar", df, order = 1)
+  preds <- c(pred$pred[1], pred$pred[2], pred$pred[3], pred$pred[4], pred$pred[5])
+  expect_equal(preds, c(1726.098, 1720.011, 1715.376, 1711.845, 1709.150),
+    tolerance = 0.01
+  )
 })
