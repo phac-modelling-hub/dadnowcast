ca5f41355a1ec82350ef53251ed6cb00a2c50989
Timothy Marsh <Timothy.D.C.Marsh@gmail.com>
2025-12-09 15:20:25 -0500

    Updated simple nowcasting

--- R/now_simple_models.R
+++ R/now_simple_models.R
@@ -1,5 +1,5 @@
 #' Nowcast from a simple linear model (internal)
-#' 
+#'
 #' @param NewX The known x values to be nowcast on.
 #' @param model The model to use for predictions.
 now_simple_lm <- function(newX, model) {
@@ -7,21 +7,29 @@ now_simple_lm <- function(newX, model) {
 }
 
 #' Nowcast from a simple AR model (internal)
-#' 
+#'
 #' @param NewX The known x values to be nowcast on.
 #' @param model The model to use for predictions.
 now_simple_arx <- function(newX, model) {
   predict(model, newxreg = newX, n.ahead = length(newX))
 }
 
+#' Return the number of NA's at the end of a vector (internal)
+#'
+#' @param y A vector of values with some number of NA's at the end of it.
+find_nas <- function(y) {
+  which(rev(y) != "NA") |>
+    min() - 1
+}
+
 #' Perform nowcasting for the specified response variable
-#' 
+#'
 #' @param formula y ~ x
 #' @param data A data frame with columns used in the formula.
 #' @param model Currently implemented: "lm" and "ar"
 #' @param order The AR order to be used in `arima()`.
-#' 
-#' @returns Predictions for the missing data in the response variable 
+#'
+#' @returns Predictions for the missing data in the response variable
 
 now_simple_models <- function(
   formula, data,
@@ -30,17 +38,17 @@ now_simple_models <- function(
 ) {
   y <- data[, all.vars(formula[[2]])]
   x <- data[, all.vars(formula[[3]])]
-  
-  naCount <- sum(is.na(y))
-  shortData <- data[1:(length(y)-naCount),]
-  
-  modToUse <- fit_simple_model(formula, shortData, model,order)
-  
+
+  naCount <- find_nas(y)
+  shortData <- data[1:(length(y) - naCount), ]
+
+  modToUse <- fit_simple_model(formula, shortData, model, order)
+
   # create predict part here that predicts naCount ahead
-  
-  newX <- x[(length(shortData[,1])+1):length(x)]
-  
-  switch (model,
+
+  newX <- x[(length(shortData[, 1]) + 1):length(x)]
+
+  switch(model,
     "lm" = now_simple_lm(newX, modToUse),
     "ar" = now_simple_arx(newX, modToUse)
   )
