1827151d22e42048046ed60543b11808c842fe7b
Devan Becker <d7becker@gmail.com>
2026-03-13 14:47:21 -0400

    Initial commit

---
+++ R/06-combine_models.R
@@ -0,0 +1,51 @@
+
+#' Add a dadnow to a multidadnow object, or combine two multidadnow objects
+#'
+#' @param dadnow1,dadnow2 A dadnow or multidadnow object.
+#'
+#' @returns A single multidadnow object.
+#' @export
+combine_dadnow <- function(dadnow1, dadnow2) {
+  if (inherits(dadnow1, "dadnow") && inherits(dadnow2, "dadnow")) {
+    return_value <- list(
+      date_col = dadnow1$date_col,
+      data = dadnow1$data,
+      models = list(dadnow1, dadnow2)
+    )
+    names(return_value$models) <- c(dadnow1$model_id, dadnow2$model_id)
+    class(return_value) <- "multidadnow"
+    return(return_value)
+  } else if (inherits(dadnow1, "multidadnow") && inherits(dadnow2, "dadnow")) {
+    models <- c(dadnow1$models, list(dadnow2))
+    names(models)[length(models)] <- dadnow2$model_id
+    return_value <- list(
+      date_col = dadnow1$date_col,
+      data = dadnow1$data,
+      models = models
+    )
+    class(return_value) <- "multidadnow"
+    return(return_value)
+  } else if (inherits(dadnow1, "dadnow") && inherits(dadnow2, "multidadnow")) {
+    models <- c(list(dadnow1), dadnow2$models)
+    names(models)[1] <- dadnow1$model_id
+    return_value <- list(
+      date_col = dadnow2$date_col,
+      data = dadnow2$data,
+      models = models
+    )
+    class(return_value) <- "multidadnow"
+    return(return_value)
+  } else if (inherits(dadnow1, "multidadnow") && inherits(dadnow2, "multidadnow")) {
+    models <- c(dadnow1$models, dadnow2$models)
+    names(models) <- c(names(dadnow1$models), names(dadnow2$models))
+    return_value <- list(
+      date_col = dadnow1$date_col,
+      data = dadnow1$data,
+      models = models
+    )
+    class(return_value) <- "multidadnow"
+    return(return_value)
+  } else {
+    stop("Both inputs must be of class 'dadnow' or 'multidadnow'.")
+  }
+}
