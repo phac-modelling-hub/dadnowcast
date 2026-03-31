38ef8d0a49d5b71fefdcbcd79c18b4c9853b5dff
Timothy Marsh <Timothy.D.C.Marsh@gmail.com>
2026-03-31 16:18:24 -0400

    Updated fit_ar to work better with the other functions, and changed tests to match the updated function

--- R/fit_ar.R
+++ R/fit_ar.R
@@ -7,7 +7,23 @@
 #'
 #' @returns Arima object, predictions, and fitted values of the model on the training data
 
-fit_ar <- function(Y_train, X_train = NULL, X_nowcast = NULL, params = list(p = 1, n.ahead = 1)) {
-  # this is just a wrapper function that calls the fit_arx model with the proper parameters
-  fit_arx(Y_train, X_train, X_nowcast, params)
+fit_ar <- function(Y_train, X_train = NULL, X_nowcast = NULL, params = list(p = 1)) {
+  p <- params$p
+  
+  n <- nrow(data.frame(X_nowcast))
+  
+  # create the AR model
+  AR_mod <- arima(Y_train, order = c(p, 0, 0))
+  
+  # create the predictions
+  preds <- predict(AR_mod, n)
+  
+  # put the predictions into a data frame and add 95% confidence bands
+  predictions <- data.frame(prediction = as.numeric(preds$pred), lower = as.numeric(preds$pred - 1.96 * preds$se), upper = as.numeric(preds$pred + 1.96 * preds$se))
+  
+  # find the fitted values of the model
+  fitVals <- Y_train - AR_mod$residuals
+  
+  # assemble the output in the correct format
+  list(model = AR_mod, prediction = predictions, fitted_values = fitVals)
 }
